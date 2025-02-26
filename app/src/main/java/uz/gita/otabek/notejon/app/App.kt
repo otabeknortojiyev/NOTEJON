4be73f4c7403b558feb85f353e461a97d854b6cb
Отабек Нортожиев <otashmotash@gmail.com>
2025-02-27 02:29:40 +0500

    test branch protection rules 2

--- app/src/main/java/uz/gita/otabek/notejon/app/App.kt
+++ app/src/main/java/uz/gita/otabek/notejon/app/App.kt
@@ -6,9 +6,4 @@ import dagger.hilt.android.HiltAndroidApp
 @HiltAndroidApp
 class App : Application() {
 
-  override fun onCreate() {
-    super.onCreate()
-  }
-
-  //hahhsahdhashdhash
 }
--- app/src/main/java/uz/gita/otabek/notejon/screens/home/items/AppInfoDialog.kt
+++ app/src/main/java/uz/gita/otabek/notejon/screens/home/items/AppInfoDialog.kt
@@ -1,9 +1,9 @@
 package uz.gita.otabek.notejon.screens.home.items
 
 import androidx.compose.foundation.Image
-import androidx.compose.material.TextButton
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
